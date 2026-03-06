b0ae0158c0c775abc665df2b2e91ded77428bda2
nurbekomar <omarnurbek13@gmail.com>
2026-03-06 14:26:59 +0500

    feat: add django-tailwind

--- config/settings.py
+++ config/settings.py
@@ -39,7 +39,12 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "apps.courses",
+    # tailwind
+    "tailwind",
+    "ui",
 ]
+
+TAILWIND_APP_NAME = "ui"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -50,6 +55,14 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+if DEBUG:
+    # Add django_browser_reload only in DEBUG mode
+    INSTALLED_APPS += ["django_browser_reload"]
+    # Add django_browser_reload middleware only in DEBUG mode
+    MIDDLEWARE += [
+        "django_browser_reload.middleware.BrowserReloadMiddleware",
+    ]
 
 ROOT_URLCONF = "config.urls"
 
@@ -117,3 +130,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+
+NPM_BIN_PATH = "C:/Program Files/nodejs/npm.cmd"
